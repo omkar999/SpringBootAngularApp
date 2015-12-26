dac1ad998ec78e8ee2142668ddab00e067b67227
omkar <omkar.p89@gmail.com>
2015-12-26 20:02:33 +0530

    first commit angular spring boot app

---
+++ src/main/java/com/myapp/config/MyAppSecurityConfig.java
@@ -0,0 +1,6 @@
+package com.myapp.config;
+
+//placeholder for security config
+public class MyAppSecurityConfig  {
+
+}
---
+++ src/main/java/com/myapp/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.myapp.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+	@Value("${name}")
+	String testname;
+	
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        System.out.println("Injected property from application.properties --------------- "+testname);
+        return "greeting";
+    }
+
+}
